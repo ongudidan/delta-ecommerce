6e6ccdffda91fba15311c5bfa8cdf496889dd13b
ongudidan <ongudidan@gmail.com>
2024-12-20 09:35:21 +0300

    modified:   config/params.php

--- config/params.php
+++ config/params.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'adminEmail' => 'danny@doubledeals.co.ke',
-    'senderEmail' => 'danny@doubledeals.co.ke',
+    'adminEmail' => 'admin@example.com',
+    'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
     'bsVersion' => '5.x',
     'bsDependencyEnabled' => false,
